75affcfb0618260ccdc80b93f769173f5db20687
moonjisu <jjisu97@naver.com>
2025-01-13 10:33:22 +0900

    exercisedream

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <>login</>;
+};
+
+export default page;
---
+++ src/app/profile/mypage/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <>Mypage</>;
+};
+
+export default page;
